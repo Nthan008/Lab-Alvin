8d2246a5d35636350963515d2003598de0bebbb2
Nthan008 <114546358+Nthan008@users.noreply.github.com>
2023-02-24 15:06:19 +0700

    Add files via upload

--- Shape/Shape.java
+++ Shape/Shape.java
@@ -1,13 +1,32 @@
-public class Shape {
+abstract class Shape {
     String color;
 
-    public Shape() {
-        color = "yellow";
-    }
+    Shape(String color) {
+        this.color = "yellow";
 
+    }
     void getArea() {
 
     }
-
 }
+class Rectangle extends Shape {
+    int width;
+    int height;
 
+    Rectangle(int width, int height, String color){
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    void getArea() {
+        System.out.println("The area for the shape is " + width * height);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Rectangle Area = new Rectangle(60,60,"yellow");
+        Area.getArea();
+    }
+}
